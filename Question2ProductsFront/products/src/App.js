c51d4418ffce724c8e529a22448baec26b56ecfd
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-06-08 12:56:08 +0530

    FEAT: Done

--- Question2ProductsFront/products/src/App.js
+++ Question2ProductsFront/products/src/App.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container, AppBar, Toolbar, Typography, makeStyles } from '@mui/material';
+import ProductList from './components/ProductList.js';
+import ProductDetail from './components/ProductDetail.js';
 
-const App = () => {
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    marginBottom: theme.spacing(3), 
+  },
+  container: {
+    marginTop: theme.spacing(6), 
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Router>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6">
+            E-Commerce App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.container}>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+        </Routes>
+      </Container>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
